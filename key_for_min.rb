e4a17ae80555d8782040bc7bcb5c0e3f1a214c5e
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 05:09:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   #key = nil
   #value = nil
    if name_hash == {}
+     return nil
   name_hash.each do |k, v|
     key = k if key == nil
     value = v if value == nil
