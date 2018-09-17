d98c770e36138f48c50bc9a604a8bf6a6512ba21
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 05:06:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,8 @@ def key_for_min_value(name_hash)
       value = name_hash.first[1]
       key = name_hash.first[0]
   name_hash.each do |k, v|
-    key = k if key == nil
-    value = v if value == nil
+    #key = k if key == nil
+    #value = v if value == nil
       if v < value 
         value = v
         key = k
